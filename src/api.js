9c996fb1e88b28fdac3b656f8a05e0d1a3d1112c
Paul <p.rossiter83@gmail.noreply.github.com>
2023-10-06 20:15:06 +0100

    Rendered data to DOM

--- src/api.js
+++ src/api.js
@@ -1,37 +1,50 @@
-import dom from './dom'
-
 const api = (() => {
     
     const apiKey = 'c19f5715236a49e6ab4144317230310';
     const apiURL = "http://api.weatherapi.com/v1/current.json?";
-    // const apiIcons = "https://www.weatherapi.com/docs/weather_conditions.json";
 
     const searchBar = document.querySelector('.search-bar');
-    const searchButton = document.querySelector('.search-button')
-
+    const searchButton = document.querySelector('.search-button');
+    const errorMessage = document.querySelector('.error-message');
+    
     async function checkWeather(city) {
         const response = await fetch(apiURL + `key=${apiKey}` + `&q=${city}`);
         const data = await response.json();
-        // const iconResponse = await fetch(apiIcons);
-        // const dataIcons = await iconResponse.json();
 
+        //Render data
         console.log(data)
+        document.querySelector('.city').textContent = `${data.location.name}`;
         console.log(data.location.name);
+
+        document.querySelector('.weather-icon').src = 'https:' + `${data.current.condition.icon}`;
         console.log(data.current.condition.icon);
+
+        document.querySelector('.condition-text').textContent = data.current.condition.text;
         console.log(data.current.condition.text);
+
+        document.querySelector('.temp').textContent = data.current.temp_c + ' °C';
         console.log(data.current.temp_c);
+
+        document.querySelector('.wind').textContent = 'Wind ' + data.current.wind_mph + ' mph';
         console.log(data.current.wind_mph);
+
+        document.querySelector('.feels-like').textContent = 'Feels like ' + data.current.feelslike_c + ' °C';
         console.log(data.current.feelslike_c);
-        console.log(data.current.precip_mm);
+        
+        document.querySelector('.precip').textContent = 'Precipitation ' + data.current.precip_in + ' inches';
+        console.log(data.current.precip_in);
+        
+        document.querySelector('.humidity').textContent = 'Humidity ' + data.current.humidity + ' %';
         console.log(data.current.humidity);
 
 
-    }
 
-    searchButton.addEventListener('click', () => {
-        checkWeather(searchBar.value)
-        console.log(searchBar.value)
-    })
+
+        searchButton.addEventListener('click', () => {
+            checkWeather(searchBar.value)
+            console.log(searchBar.value)
+        });
+    };
 
     return {
         checkWeather,

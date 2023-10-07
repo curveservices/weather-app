aa63e070e5a1167b3fc2e77475e5d74e52d865ed
Paul <p.rossiter83@gmail.noreply.github.com>
2023-10-07 20:52:45 +0100

    DOM logic removed to dom.js try/catch added to handle errors

--- src/api.js
+++ src/api.js
@@ -1,60 +1,36 @@
+import weatherUI from "./dom";
+
 const api = (() => {
   const apiKey = "c19f5715236a49e6ab4144317230310";
   const apiURL = "https://api.weatherapi.com/v1/current.json?";
-
-  const searchBar = document.querySelector(".search-bar");
-  const searchButton = document.querySelector(".search-button");
   const error = document.querySelector(".error");
   const weather = document.querySelector(".weather");
 
-  function updateDOM(data) {
-    console.log(data);
-    document.querySelector(".city").textContent = `${data.location.name}`;
+  async function fetchWeatherData(city) {
+    try {
+      const response = await fetch(apiURL + `key=${apiKey}` + `&q=${city}`);
+      if (!response.ok) {
+        weatherUI.handleError();
+        return;
+      }
+      const data = await response.json();
 
-    document.querySelector(".weather-icon").src =
-      "https:" + `${data.current.condition.icon}`;
-
-    document.querySelector(".condition-text").textContent =
-      data.current.condition.text;
-
-    document.querySelector(".temp").textContent =
-      Math.round(data.current.temp_c) + " °C";
-
-    document.querySelector(".wind").textContent =
-      "Wind " + Math.round(data.current.wind_mph) + " mph";
-
-    document.querySelector(".feels-like").textContent =
-      "Feels like " + Math.round(data.current.feelslike_c) + " °C";
-
-    document.querySelector(".precip").textContent =
-      "Precipitation " + data.current.precip_in + " inches";
-
-    document.querySelector(".humidity").textContent =
-      "Humidity " + data.current.humidity + " %";
-  }
-
-  function handleError() {
-    error.style.display = "block";
-    weather.style.display = "none";
-  }
-
-  async function checkWeather(city) {
-    const response = await fetch(apiURL + `key=${apiKey}` + `&q=${city}`);
-    if (!response.ok) {
-      handleError();
-      return;
+      weatherUI.updateDOM(data);
+      if (!data || data.loction) {
+        weatherUI.handleError();
+        return;
+      }
+      weatherUI.updateDOM(data);
+      error.style.display = "none";
+      weather.style.display = "block";
+    } catch (error) {
+      console.log("an error occured", error);
+      weatherUI.handleError();
     }
-    const data = await response.json();
-    updateDOM(data);
-    error.style.display = "none";
-    weather.style.display = "block";
   }
-  searchButton.addEventListener("click", () => {
-    checkWeather(searchBar.value);
-  });
-
+  // Return the public interface of the module
   return {
-    checkWeather,
+    fetchWeatherData,
   };
 })();
 
